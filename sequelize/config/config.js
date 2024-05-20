791b8ff5da87d5a982c9bb1b86d82db7a0b7c890
Raineecorn <janlouraine@gmail.com>
2024-05-20 17:43:56 +0800

    Initial commit with Sequelize setup

---
+++ sequelize/config/config.js
@@ -0,0 +1,31 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_HOST:', process.env.DB_HOST);
+
+module.exports = {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: "postgres",
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: "postgres"
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: "postgres"
+    }
+};
---
+++ sequelize/migrations/20240520091803-add-user-table.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { sequelize } = require('../models');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Users', 'address', {
+      type: Sequelize.STRING,
+      allowNull: true,
+      defaultValue: null,
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'address');
+  }
+};
